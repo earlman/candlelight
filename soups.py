ee23937859f4d31a998588ad0d746456382edf80
earlman <earl.mangulabnan@gmail.com>
2020-03-23 16:31:25 -0500

    initial commit

---
+++ soups.py
@@ -0,0 +1,82 @@
+import csv
+import requests
+import xml.etree.ElementTree as ET
+from bs4 import BeautifulSoup
+
+
+def parseXML(xmlfile):
+
+    # create empty list for verses
+    verses = []
+
+    # create soup
+    with open(xmlfile) as fp:
+        soup = BeautifulSoup(fp, 'xml')
+
+    # find w:r tags
+    wr = soup.find_all('w:r')
+
+    # iterate w:r items
+    for x in wr:
+
+        # check if w:r is a 'cvmarker'
+        if (x.find("w:rStyle", attrs={"w:val": "cvmarker"})):
+
+            # empty verse dictionary
+            verse = {}
+
+            # append chapter number to the list
+            verse['chNum'] = x.find("w:t").contents[0].encode('utf8')
+
+            # add verse number to list
+            vnum_ref = x.next_sibling
+            verse['vNum'] = vnum_ref.find(
+                "w:t").contents[0].encode('utf8')
+
+            # add verse text to list
+            verse_ref = vnum_ref.next_sibling
+            if verse_ref:
+                verse['text'] = verse_ref.contents[0].text.encode('utf8')
+
+            verses.append(verse)
+
+    return verses
+
+
+def savetoCSV(verses, filename):
+
+    # specifying the fields for csv file
+    fields = ['chNum', 'vNum', 'text']
+
+    # writing to csv file
+    with open(filename, 'w') as csvfile:
+
+        # creating a csv dict writer object
+        writer = csv.DictWriter(csvfile, fieldnames=fields)
+
+        # writing headers (field names)
+        writer.writeheader()
+
+        # writing data rows
+        writer.writerows(verses)
+
+
+def main():
+
+    # url of xml
+    fileUrl = 'C:/Projects/bible (python)/eng-web_word/eng-web_word.xml'
+    # fileUrl = 'C:/Projects/bible (python)/samplerssfeed.xml'
+
+    # parse xml file
+    verses = parseXML(fileUrl)
+
+    # print(verses)
+
+    # store news items in a csv file
+    savetoCSV(verses, 'bible.csv')
+
+
+if __name__ == "__main__":
+
+    # calling main function
+    main()
