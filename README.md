a8e4b4f959b26ca653e0b887e23bcc6a6f25ed42
earlman <earl.mangulabnan@gmail.com>
2020-03-27 13:32:23 -0500

    ITS ALIVEEE

---
+++ README.md
@@ -0,0 +1,5 @@
+## Start dev server
+
+```
+FLASK_APP=app.py FLASK_ENV=development flask run
+```
--- app.py
+++ app.py
@@ -1,14 +1,16 @@
 import csv
 import random
 import re
+from flask_qrcode import QRcode
 from helpers import getUrl
+from flask import Flask, render_template
 
 bible = "bible.csv"
 
 # Returns random line from bible
 
 
-def randomLine():
+def randomVerse():
     
     # Get line from CSV
     with open(bible) as f:
@@ -17,14 +19,28 @@ def randomLine():
     
     # Parse line
     verse = {}
-    verse["book"]   = chosen_row[0]
-    verse["chNum"]  = chosen_row[1]
-    verse["vNum"]   = chosen_row[2]
-    verse["text"]   = chosen_row[3]
+    verse["book"]   = chosen_row[0].decode('utf-8')
+    verse["chNum"]  = chosen_row[1].decode('utf-8')
+    verse["vNum"]   = chosen_row[2].decode('utf-8')
+    verse["text"]   = chosen_row[3].decode('utf-8')
     
+    # Create URL
+    verse["url"]    = getUrl(verse["book"], verse["chNum"])
+
     return verse
 
+def create_app():
 
+    app = Flask(__name__)
 
-verse = randomLine()
-print(getUrl(verse["book"], verse["chNum"]))
+    QRcode(app)
+
+    @app.route('/')
+    def home():
+        return render_template('index.html',
+            verse = randomVerse()
+        )
+    
+    return app
+
+create_app().run(debug=False)
